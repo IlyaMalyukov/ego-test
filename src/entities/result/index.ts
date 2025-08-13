dc82248291154c0ed925f91bbe701025995fd17f
Ilya Malyukov <imaliukov@astralinux.ru>
2025-08-13 16:37:38 +0300

    Добавлен компонент результата

---
+++ src/entities/result/index.ts
@@ -0,0 +1,3 @@
+import Result from "./ui.vue";
+
+export { Result };
---
+++ src/entities/result/ui.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { Result } from "@/shared";
+
+type Props = {
+  result: Result;
+};
+
+defineProps<Props>();
+</script>
+
+<template>
+  <div>
+    <h1> {{ result.title }} </h1>
+    <div> {{ result.description }}</div>
+
+    <ul>
+      <li v-for="advice in result.advice">
+        {{ advice }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
